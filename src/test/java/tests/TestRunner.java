b05a6616e504dad45d39687381d4fd7cf09b18c8
Rongzhu Tang <tangrongzhu@gmail.com>
2022-01-19 17:10:46 +0800

    Add files via upload

---
+++ src/test/java/tests/TestRunner.java
@@ -0,0 +1,32 @@
+package tests;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+// import com.intuit.karate.junit5.Karate;
+
+// public class TestRunner {
+//   @Karate.Test
+//     Karate testRunner() {
+//         return Karate.run("FirstTest").relativeTo(getClass());
+//     }
+
+// }
+
+class TestRunner {
+  @Test
+  void testParallel()
+  {
+    // var rb = Runner.builder();
+    // Results results = rb.parallel(1);
+
+    Results results = Runner.path("classpath:tests")
+                .tags("~@ignore")
+                //.outputCucumberJson(true)
+                .parallel(1);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+  }
+
+}
